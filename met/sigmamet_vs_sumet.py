c105273842b515fc3a805a026c2e5b47d6cf5d83
Colin <colin.bernet@cern.ch>
2016-01-13 19:07:30 +0100

    delegated code to base Fitter2D class

--- met/sigmamet_vs_sumet.py
+++ met/sigmamet_vs_sumet.py
@@ -1,6 +1,7 @@
 import sys 
 import numpy as np
 from cpyroot import *
+
 from ROOT import TLine
 
 officialStyle(gStyle)
@@ -13,49 +14,32 @@ class MetVsSumEt(Fitter2D):
         self.metname = metname
         cut = '{metname}_sumet>50.'.format(metname=metname)
         hname = '{metname}_h2d'.format(metname=metname)
-        self.h2d = TH2F(hname, hname, 
-                        nx, xmin, xmax, ny, ymin, ymax)
+        super(MetVsSumEt,self).__init__( hname, hname, 
+                                    nx, xmin, xmax, ny, ymin, ymax)
         tree.Project(self.h2d.GetName(), 
                       '{metname}_px:{metname}_sumet'.format(metname=metname),
                      cut )
         tree.Project(self.h2d.GetName(), 
                      '{metname}_py:{metname}_sumet'.format(metname=metname),
                      cut )
-        self.h2d.FitSlicesY()
-        self.hmean = gDirectory.Get( self.h2d.GetName() + '_1' )
-        self.hsigma = gDirectory.Get( self.h2d.GetName() + '_2' )
+        self.fit_slices()
         self.hsigma.SetYTitle('#sigma(MET_{x,y})')
-        self.hchi2 = gDirectory.Get( self.h2d.GetName() + '_chi2' )
-
-    def format(self, style):
-        for hist in [self.hmean, self.hsigma, self.hchi2]: 
-            style.format(hist)
-            hist.SetTitle('')
-            hist.SetXTitle('#SigmaE_{T} (GeV)')
-
+ 
         
 class SumEtResponse(Fitter2D): 
     def __init__(self, metname, tree, nx, xmin, xmax, ny, ymin, ymax):
         self.metname = metname
         cut = 'genMetTrue_sumet>50.'.format(metname=metname)
         hname = '{metname}_SumEtResponse_h2d'.format(metname=metname)
-        self.h2d = TH2F(hname, hname, 
-                        nx, xmin, xmax, ny, ymin, ymax)
+        super(SumEtResponse,self).__init__( hname, hname, 
+                                    nx, xmin, xmax, ny, ymin, ymax)
         tree.Project(self.h2d.GetName(), 
-                      '{metname}_sumet/genMetTrue_sumet:genMetTrue_sumet'.format(metname=metname),
+                     '{metname}_sumet/genMetTrue_sumet:genMetTrue_sumet'.format(metname=metname),
                      cut )
-        self.h2d.FitSlicesY()
-        self.hmean = gDirectory.Get( self.h2d.GetName() + '_1' )
+        self.fit_slices()
         self.hmean.SetYTitle('#SigmaE_{T} response')
-        self.hsigma = gDirectory.Get( self.h2d.GetName() + '_2' )
-        self.hsigma.SetYTitle('#sigma(MET_{x,y})')
         self.hchi2 = gDirectory.Get( self.h2d.GetName() + '_chi2' )
 
-    def format(self, style):
-        for hist in [self.hmean, self.hsigma, self.hchi2]: 
-            style.format(hist)
-            hist.SetTitle('')
-            hist.SetXTitle('true #SigmaE_{T} (GeV)')
  
         
 
@@ -64,13 +48,13 @@ xmin, xmax = 0, 2000
 args = (tree, 50, xmin, xmax, 100, -100, 100)
 
 metcalo = MetVsSumEt('caloMetT1', *args) 
-metcalo.format(traditional_style)
+metcalo.format(traditional_style, '#SigmaE_{T} (GeV)')
 metcalo.hsigma.Draw()
 # metcalo.hsigma.GetYaxis().SetRangeUser(0,30)
 # metcalo.hsigma.GetXaxis().SetNdivisions(10)
 
 metpf = MetVsSumEt('pfMetT1', *args) 
-metpf.format(pf_style)
+metpf.format(pf_style, '#SigmaE_{T} (GeV)')
 metpf.hsigma.Draw('same')
 
 legend = TLegend(0.21,0.79, 0.49, 0.92)
@@ -84,11 +68,11 @@ c2 = TCanvas("c2","c2")
 sumetresponse_args = (tree, 50, 0, 2000, 100, 0, 2)
 
 sumetresponse_pf = SumEtResponse('pfMetT1', *sumetresponse_args) 
-sumetresponse_pf.format(pf_style)
+sumetresponse_pf.format(pf_style, '#SigmaE_{T} (GeV)')
 sumetresponse_pf.hmean.Draw()
 
 sumetresponse_calo = SumEtResponse('caloMetT1', *sumetresponse_args) 
-sumetresponse_calo.format(traditional_style)
+sumetresponse_calo.format(traditional_style, '#SigmaE_{T} (GeV)')
 sumetresponse_calo.hmean.Draw("same")
 
 sumetresponse_pf.hmean.GetYaxis().SetRangeUser(0.5,1.2)
