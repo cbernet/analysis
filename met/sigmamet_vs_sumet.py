4aba47b7218dc1331d0cf4c96a946b9c13116382
Colin <colin.bernet@cern.ch>
2016-04-08 12:52:38 +0200

    extended range for response plot as well

--- met/sigmamet_vs_sumet.py
+++ met/sigmamet_vs_sumet.py
@@ -28,12 +28,11 @@ class MetVsSumEt(Fitter2D):
          
         
 class SumEtResponse(Fitter2D): 
-    def __init__(self, metname, tree, nx, xmin, xmax, ny, ymin, ymax):
+    def __init__(self, metname, tree, *args):
         self.metname = metname
         cut = 'genMetTrue_sumet>50.'.format(metname=metname)
         hname = '{metname}_SumEtResponse_h2d'.format(metname=metname)
-        super(SumEtResponse,self).__init__( hname, hname, 
-                                            nx, xmin, xmax, ny, ymin, ymax)
+        super(SumEtResponse,self).__init__( hname, hname, *args)
         tree.Project(self.h2d.GetName(), 
                      '{metname}_sumet/genMetTrue_sumet:genMetTrue_sumet'.format(metname=metname),
                      cut, '', NEVENTS)
@@ -81,12 +80,13 @@ tdrstyle.cmsPrel(-1, 13, True)
 c1.SaveAs('met_sigma_vs_sumet.pdf')
 
 c2 = TCanvas("c2","c2")
-xmin, xmax = 0, 1900
-sumetresponse_args = (tree, 50, xmin, xmax, 100, 0, 2)
+# sumetresponse_args = (tree, 50, xmin, xmax, 100, 0, 2)
+sumetresponse_args = (tree, len(xbins)-1, xbins, 100, 0, 2)
 
 sumetresponse_pf = SumEtResponse('pfMetT1', *sumetresponse_args) 
 sumetresponse_pf.format(pf_style, '#SigmaE_{T} (GeV)')
 sumetresponse_pf.hmean.Draw()
+sumetresponse_pf.hmean.GetXaxis().SetNdivisions(5)
 
 sumetresponse_calo = SumEtResponse('caloMetT1', *sumetresponse_args) 
 sumetresponse_calo.format(traditional_style, '#SigmaE_{T} (GeV)')
