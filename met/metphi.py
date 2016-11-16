ec2a2722b29240d89381411908ad130dee8f760c
Colin <colin.bernet@cern.ch>
2016-11-16 15:53:04 +0100

    variable binning in met phi resolution plot

--- met/metphi.py
+++ met/metphi.py
@@ -1,5 +1,6 @@
 import sys 
 from cpyroot import *
+import numpy as np
 
 from ROOT import TLine
 
@@ -10,12 +11,11 @@ tree = rootfile.Get('events')
 
 class MetPhiVsSumEt(Fitter2D):
     
-    def __init__(self, metname, tree, nx, xmin, xmax, ny, ymin, ymax):
+    def __init__(self, metname, tree, *args):
         self.metname = metname
         cut = 'genMetTrue_pt>50.'.format(metname=metname)
         hname = '{metname}_MetPhi_h2d'.format(metname=metname)
-        super(MetPhiVsSumEt,self).__init__( hname, hname, 
-                                            nx, xmin, xmax, ny, ymin, ymax)
+        super(MetPhiVsSumEt,self).__init__( hname, hname, *args)
         tree.Project(self.h2d.GetName(), 
                      '{metname}_phi-genMetTrue_phi:genMetTrue_pt'.format(metname=metname),
                      cut )
@@ -24,7 +24,16 @@ class MetPhiVsSumEt(Fitter2D):
 
 c1 = TCanvas("c1","c1")        
 xmin, xmax = 50, 250
-args = (tree, 20, xmin, xmax, 100, -1, 1)
+
+binsize_low = 10
+xbins_low_max = 180
+xbins_low = np.linspace(xmin, xbins_low_max, (xbins_low_max-xmin)/binsize_low+1)
+xbins_up = np.array([180,200, 220, xmax])
+# xbins_up = np.array([180, 190, 200, 210, 220, 230, 240, 250])
+xbins = np.concatenate((xbins_low, xbins_up))
+print xbins
+
+args = (tree, len(xbins)-1, xbins, 100, -1, 1)
 
 metcalo = MetPhiVsSumEt('caloMetT1', *args) 
 metcalo.format(traditional_style, 'True E_{T}^{miss} (GeV)')
@@ -40,7 +49,7 @@ legend.AddEntry(metcalo.hsigma, 'Calo', 'p')
 legend.AddEntry(metpf.hsigma, 'PF', 'p')
 legend.Draw('same')
 
-tdrstyle.cmsPrel(-1, 13, True)
+tdrstyle.cmsPrel(-1, None, True)
 
 c1.SaveAs('met_phi_vs_truemet.pdf')
 
